924273233e01ec209f43592d8cdc28e72f753e7f
Valentin Ouvrard <valentin.ouvrard@yahoo.fr>
2019-07-31 21:47:43 +0200

    fix(update): Fix update bugs

--- src/App.js
+++ src/App.js
@@ -2,23 +2,24 @@
 import React from 'react'
 import { ContextProvider } from './context'
 import WithThemes from './theme'
-import Typography from '@material-ui/core/Typography'
 import styled from 'styled-components'
 
-class App extends React.Component {
-
-  render() {
-    return (
-      <ContextProvider>
-        <WithThemes>
-          <CustomTypo>Starter</CustomTypo>
-        </WithThemes>
-    </ContextProvider>)
-  }
-}
-
-const CustomTypo = styled(Typography)`
-  background: ${props => props.theme.mui.palette.primary.main};
+const MainContainer = styled.div`
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  width: 100%;
+  min-height: 100%;
+  margin: 0;
+  position: absolute;
+  top: 0;
+  left: 0;
 `
-
+function App() {
+  return (
+    <ContextProvider>
+      <WithThemes>
+        <MainContainer>
+        </MainContainer>
+      </WithThemes>
+    </ContextProvider>)
+}
 export default App
