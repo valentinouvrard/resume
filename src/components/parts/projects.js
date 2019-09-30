ad69f1a7eed5cf2dbb4eb94b60d7d0f8896862c0
Valentin Ouvrard <valentin.ouvrard@yahoo.fr>
2019-09-30 15:02:26 +0200

    projects: add links and details

--- src/components/parts/projects.js
+++ src/components/parts/projects.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import { faLink } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import CustomFaIcon from '../decoration/custom-fa-icon';
 const Container = styled.div`
   display: block;
   width: 100%;
@@ -8,7 +11,6 @@ const Container = styled.div`
 
 const Title = styled.h2`
   font-weight: 400;
-  margin-bottom: 3mm;
 `
 
 const SubContainer = styled.div`
@@ -18,9 +20,8 @@ const SubContainer = styled.div`
 const ProjectTitle = styled.h5`
   color: ${props => props.theme.palette.greys.dark};
   display: inline-block;
-  margin-right: 4mm;
-  margin-bottom: 1mm;
   font-weight: 600;
+  margin-right: 4mm;
 `
 const ProjectSubInfos = styled.p`
   font-weight: 400;
@@ -33,6 +34,27 @@ const ProjectTechs = styled(ProjectSubInfos)`
   font-weight: 150;
 `
 
+const Details = styled.div`
+  margin-left: 2rem;
+`
+
+
+const SubInfos = styled.p`
+  font-weight: 200;
+  color: ${props => props.theme.palette.greys.normal};
+`
+
+const InfoLink = styled.a`
+  font-size: 0.7rem;
+`
+
+const NewTabLink = (props) => <InfoLink target="_blank" rel="noopener noreferrer" href={props.href}> {props.children} </InfoLink>
+
+const ProjectLink = styled.div`
+  margin-right: 4mm;
+  display: inline-block;
+`
+
 function Projects() {
   return (
     <Container>
@@ -40,17 +62,37 @@ function Projects() {
       <SubContainer>
         <ProjectTitle>Bananup</ProjectTitle>
         <ProjectTechs>Django, React</ProjectTechs>
-        <ProjectSubInfos>A website made for semi-competitive gamers that want to improve their gaming skills thanks to community-driven feedbacks.</ProjectSubInfos>
-      </SubContainer>
-      <SubContainer>
-        <ProjectTitle> NetflixRoulette </ProjectTitle>
-        <ProjectTechs>Django, React</ProjectTechs>
-        <ProjectSubInfos>A website made for french Netflix users. It allows you to choose randomly in the french catalog. For now, nothing more than categories is offered as filters.</ProjectSubInfos>
+        <div>
+          <ProjectLink>
+            <CustomFaIcon icon={faLink} size="xs"/>
+            <NewTabLink href="https://bananup.co/">bananup.co</NewTabLink>
+          </ProjectLink>
+          <ProjectLink>
+            <CustomFaIcon icon={faGithub} size="xs"/>
+            <NewTabLink href="https://github.com/valentinouvrard/bananup-front">Bananup-front</NewTabLink>
+          </ProjectLink>
+        </div>
+        <ProjectSubInfos>In the context of Epita's StartupLab, we created a website made for semi-competitive gamers that want to improve their gaming skills thanks to community-driven feedbacks.</ProjectSubInfos>
+        <Details>
+          <SubInfos>- Creating a startup project</SubInfos>
+          <SubInfos>- Learning the concept of MVP</SubInfos>
+        </Details>
       </SubContainer>
       <SubContainer>
         <ProjectTitle> Sportizen </ProjectTitle>
         <ProjectTechs>NodeJs, React Native</ProjectTechs>
+        <div>
+          <ProjectLink>
+            <CustomFaIcon icon={faGithub} size="xs"/>
+            <NewTabLink href="https://github.com/valentinouvrard/sportizen-app">Sportizen-app</NewTabLink>
+          </ProjectLink>
+        </div>
         <ProjectSubInfos>An application that allows users to generate sport activities and keep track of their performances.</ProjectSubInfos>
+        <Details>
+          <SubInfos>- Learning cross-platform mobile development with React Native</SubInfos>
+          <SubInfos>- Facing geolocation problematics on mobile devices</SubInfos>
+          <SubInfos>- Organizing a 1 year long project with a team of 4</SubInfos>
+        </Details>
       </SubContainer>
     </Container>
   )
